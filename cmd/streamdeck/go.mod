6bbb7dce111a661bfd9a627eb6024dc442589062
Knut Ahlers <knut@ahlers.me>
2019-11-22 14:00:20 +0100

    Prevent write collisions
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- cmd/streamdeck/go.mod
+++ cmd/streamdeck/go.mod
@@ -6,11 +6,15 @@ replace github.com/Luzifer/streamdeck => ../../
 
 require (
 	github.com/Luzifer/rconfig/v2 v2.2.1
-	github.com/Luzifer/streamdeck v0.0.0-20191122001547-bfc4857847cb
+	github.com/Luzifer/streamdeck v0.0.0-20191122003228-a2f524a6b22c
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/pkg/errors v0.8.1
 	github.com/sashko/go-uinput v0.0.0-20180923134002-15fcac7aa54a
 	github.com/sirupsen/logrus v1.4.2
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8
+	golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e // indirect
+	gopkg.in/validator.v2 v2.0.0-20191107172027-c3144fdedc21 // indirect
 	gopkg.in/yaml.v2 v2.2.7
 )
--- deck_original_v2.go
+++ deck_original_v2.go
@@ -7,6 +7,7 @@ import (
 	"image/color"
 	"image/jpeg"
 	"strings"
+	"sync"
 
 	"github.com/disintegration/imaging"
 	"github.com/pkg/errors"
@@ -19,7 +20,8 @@ const (
 )
 
 type deckConfigOriginalV2 struct {
-	dev *hid.Device
+	dev       *hid.Device
+	writeLock sync.Mutex
 
 	keyState []EventType
 }
@@ -33,19 +35,19 @@ func newDeckConfigOriginalV2() *deckConfigOriginalV2 {
 
 func (d *deckConfigOriginalV2) SetDevice(dev *hid.Device) { d.dev = dev }
 
-func (d deckConfigOriginalV2) NumKeys() int                     { return 15 }
-func (d deckConfigOriginalV2) KeyColumns() int                  { return 5 }
-func (d deckConfigOriginalV2) KeyRows() int                     { return 3 }
-func (d deckConfigOriginalV2) KeyDirection() keyDirection       { return keyDirectionLTR }
-func (d deckConfigOriginalV2) KeyDataOffset() int               { return 4 }
-func (d deckConfigOriginalV2) TransformKeyIndex(keyIdx int) int { return keyIdx }
+func (d *deckConfigOriginalV2) NumKeys() int                     { return 15 }
+func (d *deckConfigOriginalV2) KeyColumns() int                  { return 5 }
+func (d *deckConfigOriginalV2) KeyRows() int                     { return 3 }
+func (d *deckConfigOriginalV2) KeyDirection() keyDirection       { return keyDirectionLTR }
+func (d *deckConfigOriginalV2) KeyDataOffset() int               { return 4 }
+func (d *deckConfigOriginalV2) TransformKeyIndex(keyIdx int) int { return keyIdx }
 
-func (d deckConfigOriginalV2) IconSize() int  { return 72 }
-func (d deckConfigOriginalV2) IconBytes() int { return d.IconSize() * d.IconSize() * 3 }
+func (d *deckConfigOriginalV2) IconSize() int  { return 72 }
+func (d *deckConfigOriginalV2) IconBytes() int { return d.IconSize() * d.IconSize() * 3 }
 
-func (d deckConfigOriginalV2) Model() uint16 { return StreamDeckOriginalV2 }
+func (d *deckConfigOriginalV2) Model() uint16 { return StreamDeckOriginalV2 }
 
-func (d deckConfigOriginalV2) FillColor(keyIdx int, col color.RGBA) error {
+func (d *deckConfigOriginalV2) FillColor(keyIdx int, col color.RGBA) error {
 	img := image.NewRGBA(image.Rect(0, 0, d.IconSize(), d.IconSize()))
 
 	for x := 0; x < d.IconSize(); x++ {
@@ -57,7 +59,10 @@ func (d deckConfigOriginalV2) FillColor(keyIdx int, col color.RGBA) error {
 	return d.FillImage(keyIdx, img)
 }
 
-func (d deckConfigOriginalV2) FillImage(keyIdx int, img image.Image) error {
+func (d *deckConfigOriginalV2) FillImage(keyIdx int, img image.Image) error {
+	d.writeLock.Lock()
+	defer d.writeLock.Unlock()
+
 	buf := new(bytes.Buffer)
 
 	// We need to rotate the image or it will be presented upside down
@@ -96,7 +101,7 @@ func (d deckConfigOriginalV2) FillImage(keyIdx int, img image.Image) error {
 	return nil
 }
 
-func (d deckConfigOriginalV2) FillPanel(img image.RGBA) error {
+func (d *deckConfigOriginalV2) FillPanel(img image.RGBA) error {
 	if img.Bounds().Size().X < d.KeyColumns()*d.IconSize() || img.Bounds().Size().Y < d.KeyRows()*d.IconSize() {
 		return errors.New("Image is too small")
 	}
@@ -115,11 +120,11 @@ func (d deckConfigOriginalV2) FillPanel(img image.RGBA) error {
 	return nil
 }
 
-func (d deckConfigOriginalV2) ClearKey(keyIdx int) error {
+func (d *deckConfigOriginalV2) ClearKey(keyIdx int) error {
 	return d.FillColor(keyIdx, color.RGBA{0x0, 0x0, 0x0, 0xff})
 }
 
-func (d deckConfigOriginalV2) ClearAllKeys() error {
+func (d *deckConfigOriginalV2) ClearAllKeys() error {
 	for i := 0; i < d.NumKeys(); i++ {
 		if err := d.ClearKey(i); err != nil {
 			return errors.Wrap(err, "Unable to clear key")
@@ -128,7 +133,7 @@ func (d deckConfigOriginalV2) ClearAllKeys() error {
 	return nil
 }
 
-func (d deckConfigOriginalV2) SetBrightness(pct int) error {
+func (d *deckConfigOriginalV2) SetBrightness(pct int) error {
 	if pct < 0 || pct > 100 {
 		return errors.New("Percentage out of bounds")
 	}
@@ -143,7 +148,7 @@ func (d deckConfigOriginalV2) SetBrightness(pct int) error {
 	return errors.Wrap(err, "Unable to send feature report")
 }
 
-func (d deckConfigOriginalV2) ResetToLogo() error {
+func (d *deckConfigOriginalV2) ResetToLogo() error {
 	_, err := d.dev.SendFeatureReport([]byte{
 		0x03,
 		0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -155,7 +160,7 @@ func (d deckConfigOriginalV2) ResetToLogo() error {
 	return errors.Wrap(err, "Unable to send feature report")
 }
 
-func (d deckConfigOriginalV2) GetFimwareVersion() (string, error) {
+func (d *deckConfigOriginalV2) GetFimwareVersion() (string, error) {
 	fw := make([]byte, 32)
 	fw[0] = 5
 
