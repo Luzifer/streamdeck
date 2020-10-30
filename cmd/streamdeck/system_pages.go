83122bff42fa1da310327200dfaa8ac0eda8c44f
Knut Ahlers <knut@ahlers.me>
2020-10-30 20:00:18 +0100

    Fix blank page not doing anything
    refs #2
    
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- cmd/streamdeck/system_pages.go
+++ cmd/streamdeck/system_pages.go
@@ -1,6 +1,26 @@
 package main
 
 func applySystemPages(conf *config) {
-	blankPage := page{}
+	blankPage := page{Keys: make(map[int]keyDefinition)}
+
+	for i := 0; i < sd.NumKeys(); i++ {
+		blankPage.Keys[i] = keyDefinition{
+			Display: dynamicElement{
+				Type: "color",
+				Attributes: map[string]interface{}{
+					"rgba": []interface{}{0x0, 0x0, 0x0, 0xff},
+				},
+			},
+			Actions: []dynamicElement{
+				{
+					Type: "page",
+					Attributes: map[string]interface{}{
+						"relative": 1,
+					},
+				},
+			},
+		}
+	}
+
 	conf.Pages["@@blank"] = blankPage
 }
