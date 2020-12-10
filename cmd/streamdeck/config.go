e519e362828289593fe022fb95f42b3a897be353
Knut Ahlers <knut@ahlers.me>
2020-12-10 18:19:55 +0100

    Add json definition for config fields
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- cmd/streamdeck/config.go
+++ cmd/streamdeck/config.go
@@ -11,35 +11,35 @@ import (
 const defaultLongPressDuration = 500 * time.Millisecond
 
 type config struct {
-	AutoReload        bool            `yaml:"auto_reload"`
-	CaptionBorder     int             `yaml:"caption_border"`
-	CaptionColor      [4]int          `yaml:"caption_color"`
-	CaptionFont       string          `yaml:"caption_font"`
-	CaptionFontSize   float64         `yaml:"caption_font_size"`
-	CaptionPosition   captionPosition `yaml:"caption_position"`
-	DefaultBrightness int             `yaml:"default_brightness"`
-	DefaultPage       string          `yaml:"default_page"`
-	DisplayOffTime    time.Duration   `yaml:"display_off_time"`
-	LongPressDuration time.Duration   `yaml:"long_press_duration"`
-	Pages             map[string]page `yaml:"pages"`
-	RenderFont        string          `yaml:"render_font"`
+	AutoReload        bool            `json:"auto_reload" yaml:"auto_reload"`
+	CaptionBorder     int             `json:"caption_border" yaml:"caption_border"`
+	CaptionColor      [4]int          `json:"caption_color" yaml:"caption_color"`
+	CaptionFont       string          `json:"caption_font" yaml:"caption_font"`
+	CaptionFontSize   float64         `json:"caption_font_size" yaml:"caption_font_size"`
+	CaptionPosition   captionPosition `json:"caption_position" yaml:"caption_position"`
+	DefaultBrightness int             `json:"default_brightness" yaml:"default_brightness"`
+	DefaultPage       string          `json:"default_page" yaml:"default_page"`
+	DisplayOffTime    time.Duration   `json:"display_off_time" yaml:"display_off_time"`
+	LongPressDuration time.Duration   `json:"long_press_duration" yaml:"long_press_duration"`
+	Pages             map[string]page `json:"pages" yaml:"pages"`
+	RenderFont        string          `json:"render_font" yaml:"render_font"`
 }
 
 type page struct {
-	Keys     map[int]keyDefinition `yaml:"keys"`
-	Overlay  string                `yaml:"overlay"`
-	Underlay string                `yaml:"underlay"`
+	Keys     map[int]keyDefinition `json:"keys" yaml:"keys"`
+	Overlay  string                `json:"overlay" yaml:"overlay"`
+	Underlay string                `json:"underlay" yaml:"underlay"`
 }
 
 type keyDefinition struct {
-	Display dynamicElement   `yaml:"display"`
-	Actions []dynamicElement `yaml:"actions"`
+	Display dynamicElement   `json:"display" yaml:"display"`
+	Actions []dynamicElement `json:"actions" yaml:"actions"`
 }
 
 type dynamicElement struct {
-	Type       string                 `yaml:"type"`
-	LongPress  bool                   `yaml:"long_press"`
-	Attributes map[string]interface{} `yaml:"attributes"`
+	Type       string                 `json:"type" yaml:"type"`
+	LongPress  bool                   `json:"long_press" yaml:"long_press"`
+	Attributes map[string]interface{} `json:"attributes" yaml:"attributes"`
 }
 
 func newConfig() config {
